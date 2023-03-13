682c2e5568e3b70c56ab2f56455127775e86913d
jurogrammer <solution_12@khu.ac.kr>
2023-03-14 00:10:16 +0900

    init

---
+++ src/main/java/juro/vuestudy/VuestudyApplication.java
@@ -0,0 +1,13 @@
+package juro.vuestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuestudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuestudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/juro/vuestudy/VuestudyApplicationTests.java
@@ -0,0 +1,13 @@
+package juro.vuestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuestudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
